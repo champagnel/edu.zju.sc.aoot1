fd46a301432aa4d881adb9ba1e18f461fcee457d
hs <173500101@qq.com>
2020-11-11 10:37:01 +0800

    第一次提交

--- src/test/java/TestDie.java
+++ src/test/java/TestDie.java
@@ -1,3 +1,4 @@
+import com.sun.org.apache.xerces.internal.xs.XSIDCDefinition;
 import edu.zju.sc.aoot.Die;
 import junit.framework.Assert;
 import org.junit.Test;
@@ -9,5 +10,11 @@ public class TestDie {
 		Die die = new Die(6);
 		int temp = die.numOfSide();
 		Assert.assertEquals(temp, 6);
+	}
+	@Test
+	public void testDie(){
+		Die die = new Die();
+		int side = die.numOfSide();
+		Assert.assertEquals(side, 6);
 	}
 }
